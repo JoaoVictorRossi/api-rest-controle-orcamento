c654e926decbf77de9fbcd9cf8308a284bd6f008
João Victor Rossi <jvrossi204@gmail.com>
2022-12-04 13:09:28 -0300

    Criação do Projeto

---
+++ src/main/java/com/challenge/alura/orcamento/api/ControleDeOrcamentoFamiliarApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.alura.orcamento.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeOrcamentoFamiliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeOrcamentoFamiliarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/alura/orcamento/api/ControleDeOrcamentoFamiliarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.alura.orcamento.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeOrcamentoFamiliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
